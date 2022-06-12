1d4fa290d8f5965d32fd7485cab0a3aa7fddbee3
xaiur <syabilkahfialfatih@gmail.com>
2022-06-12 20:46:04 +0800

    read & write on node.js

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -5,3 +5,8 @@ const fs = require('fs');
 
 const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
 console.log(textIn);
+
+const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${new Date()}.\n`;
+fs.writeFileSync('./starter/txt/output.txt', textOut, 'utf-8');
+
+console.log('File Written');
