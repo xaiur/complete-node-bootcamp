6e2ceadba19927206ad36ae5e5f62d8a5ca76fe1
xaiur <syabilkahfialfatih@gmail.com>
2022-06-14 09:47:28 +0800

    menampilkan informasi di node

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const http = require('http');
+
+//--------------------------------------------FILES-------------------------------------------------//
 
 // const hello = 'Hello World';
 // console.log(hello);
@@ -10,24 +13,33 @@ const fs = require('fs');
 // fs.writeFileSync('./starter/txt/output.txt', textOut, 'utf-8');
 // console.log('File Written');
 
-// Non-Bloking, asynchronous way
+// // Non-Bloking, asynchronous way
+// fs.readFile('./starter/txt/startttt.txt', 'utf8', (err, data1) => {
+//   if (err) return console.error('error! 💥');
+//   //baris ini pakai Backtick
+//   fs.readFile(`./starter/txt/${data1}.txt`, 'utf8', (err, data2) => {
+//     fs.readFile(`./starter/txt/append.txt`, 'utf8', (err, data3) => {
+//       console.log(data3);
 
-fs.readFile('./starter/txt/startttt.txt', 'utf8', (err, data1) => {
-  if (err) return console.error('error! 💥');
-  //baris ini pakai Backtick
-  fs.readFile(`./starter/txt/${data1}.txt`, 'utf8', (err, data2) => {
-    fs.readFile(`./starter/txt/append.txt`, 'utf8', (err, data3) => {
-      console.log(data3);
+//       fs.writeFile(
+//         './starter/txt/final.txt',
+//         `${data2}\n${data3}`,
+//         'utf8',
+//         (err) => {
+//           console.log('Your file has been written successfully 😃');
+//         }
+//       );
+//     });
+//   });
+// });
+// console.log('Will Read File!💨');
 
-      fs.writeFile(
-        './starter/txt/final.txt',
-        `${data2}\n${data3}`,
-        'utf8',
-        (err) => {
-          console.log('Your file has been written successfully 😃');
-        }
-      );
-    });
-  });
+//--------------------------------------------SERVER-------------------------------------------------//
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end('Hello from server!');
 });
-console.log('Will Read File!💨');
+
+server.listen(8000, '127.0.0.1', () =>
+  console.log('Server listening on port 8000')
+);
