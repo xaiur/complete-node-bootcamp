ada5cd548dd98f9f60c3e119b0c576f3e3bdea74
xaiur <syabilkahfialfatih@gmail.com>
2022-06-15 11:52:23 +0800

    15.HTML Templating: Filling the Templates

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -38,19 +38,62 @@ const url = require('url');
 //--------------------------------------------SERVER-------------------------------------------------//
 
 //kodenya sync karena hanya dieksekusi sekali di awal, makan tidak masalah funsinya membloking karena top level code yang di jalankan sekali, penting untuk mengetahui kode mana yang hanya dijalankan sekali, dan kode mana yang di jalankan berulang.
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8'); //fungsi dirname untuk bisa masuk ke file yang dituju
+//fungsi dirname untuk bisa masuk ke file yang dituju
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%ID%}/g, product.ID);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   // console.log(req.url);
   const pathName = req.url;
+
+  //--------------------------------------------OVERVIEW-PAGE------------------------------------------//
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the Overview!');
+    res.writeHead(200, { 'content-type': 'text/html' });
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARD%}', cardHtml);
+    res.end(output);
+
+    //-------------------------------------------PRODUCT-PAGE------------------------------------------//
   } else if (pathName === '/product') {
-    res.end('This is the Product!');
+    res.end(tempProduct);
+
+    //------------------------------------------------API----------------------------------------------//
   } else if (pathName === '/api') {
     res.writeHead(200, { 'content-type': 'application/json' });
     res.end(data);
+
+    //---------------------------------------------NOT-FOUND-------------------------------------------//
   } else {
     res.writeHead(404, {
       'Content-Type': 'text/html',
