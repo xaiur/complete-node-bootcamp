43764a4651d4aeb6f674671773e505847360d17c
xaiur <syabilkahfialfatih@gmail.com>
2022-06-15 18:37:03 +0800

    16.Parsing Variables from URLs

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -72,28 +72,33 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   //--------------------------------------------OVERVIEW-PAGE------------------------------------------//
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
-    const cardHtml = dataObj
+    const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
       .join('');
-    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHtml);
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     res.end(output);
 
     //-------------------------------------------PRODUCT-PAGE------------------------------------------//
-  } else if (pathName === '/product') {
-    res.writeHead(200, {
-      'Content-type': 'text/html',
-    });
+  } else if (pathname === '/product') {
     const product = dataObj[query.id];
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
+    // console.log(query);
+    // res.writeHead(200, {
+    //   'Content-type': 'text/html',
+    // });
+    // const product = dataObj[query.id];
+    // const output = replaceTemplate(tempProduct, product);
+    // res.end(output);
 
     //------------------------------------------------API----------------------------------------------//
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
 
