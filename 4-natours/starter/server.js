b5f738e4387a96028c4a212b564efa8da8b544cd
xaiur <syabilkahfialfatih@gmail.com>
2022-08-02 18:25:16 +0800

    122. Errors Outside Express: Unhandled Rejections

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -23,6 +23,14 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLER REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
