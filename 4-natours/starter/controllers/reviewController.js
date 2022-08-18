543d1f0b6a6c4c2b27aad30dfc1b33e18319b8ef
xaiur <syabilkahfialfatih@gmail.com>
2022-08-18 14:36:44 +0800

    158. Implementing Simple Nested Routes

--- 4-natours/starter/controllers/reviewController.js
+++ 4-natours/starter/controllers/reviewController.js
@@ -15,8 +15,11 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
-  const NewReview = await Review.create(req.body);
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
 
+  const NewReview = await Review.create(req.body);
   res.status(200).json({
     status: 'success',
     data: {
--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
+
 const router = express.Router();
 
 router.post('/signup', authController.signup);
@@ -29,5 +30,6 @@ router
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
+
 
 module.exports = router;
