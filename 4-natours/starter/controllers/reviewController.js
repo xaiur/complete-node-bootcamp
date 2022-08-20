efc7d0897c1100290058364b55c691bdc739db50
xaiur <syabilkahfialfatih@gmail.com>
2022-08-20 11:21:03 +0800

    163. Factory Functions: Reading

--- 4-natours/starter/controllers/reviewController.js
+++ 4-natours/starter/controllers/reviewController.js
@@ -3,20 +3,7 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
 
-  const review = await Review.find(filter);
-
-  res.status(200).json({
-    status: 'success',
-    results: review.length,
-    data: {
-      review,
-    },
-  });
-});
 exports.setTourUserIds = catchAsync(async (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = await req.params.tourId;
@@ -25,6 +12,8 @@ exports.setTourUserIds = catchAsync(async (req, res, next) => {
   next();
 });
 
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -11,19 +11,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -60,13 +47,6 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     data: null,
   });
 });
-
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    messege: 'This route is not yet define!',
-  });
-};
 exports.updateUser = (req, res) => {
   res.status(500).json({
     status: 'error',
@@ -76,9 +56,12 @@ exports.updateUser = (req, res) => {
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    messege: 'This route is not yet define!',
+    messege: 'This route is not define! Please use /signup instead',
   });
 };
+
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
 
 // Do NOT update passwords with this!
 exports.updateUser = factory.updateOne(User);
