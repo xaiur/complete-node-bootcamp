a4d4d613d21a43dc2a8b39768eacfb46522f7a5f
xaiur <syabilkahfialfatih@gmail.com>
2022-08-20 17:01:10 +0800

    165. Adding Missing Authentication and Authorization

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -5,7 +5,6 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const sendEmail = require('./../utils/email');
-const { appendFile } = require('fs');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
