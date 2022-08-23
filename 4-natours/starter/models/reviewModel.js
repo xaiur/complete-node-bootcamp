19b7d5c6b54d54f5b2b69d18de42a3471d948317
xaiur <syabilkahfialfatih@gmail.com>
2022-08-23 20:51:28 +0800

    170. Preventing Duplicate Reviews

--- 4-natours/starter/models/reviewModel.js
+++ 4-natours/starter/models/reviewModel.js
@@ -36,7 +36,7 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
-// reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
